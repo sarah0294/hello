777b71e538827ea3834d23e2ac65746264e2d221
sara-delacasa-tierno <sara.delacasalanza@tiernoparla.es>
2022-09-27 18:42:48 +0200

    cambio 4

---
+++ ejercicioselementales/src/main/java/com/tiernoparla/ejercicioselementales/ejercicioselementales.java
@@ -0,0 +1,9 @@
+
+package com.tiernoparla.ejercicioselementales;
+
+public class ejercicioselementales {
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }//main
+    
+}//ejercicioselementales
